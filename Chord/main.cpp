db8cdaa12baee9642cd831595a893be8f81c807f
Asjad Athick <asjadathick@gmail.com>
2017-04-26 14:25:09 +1000

    Initial Commit

---
+++ Chord/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Chord
+//
+//  Created by Telstra on 26/4/17.
+//  Copyright © 2017 asjad.io. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
